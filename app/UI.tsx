67891d8426b8b1be2c6a47625bd2bcdedcc44c0d
Manchester-United-in-my-heart <101206138+Manchester-United-in-my-heart@users.noreply.github.com>
2023-08-01 20:47:36 +0700

    done UI

--- app/UI.tsx
+++ app/UI.tsx
@@ -1,76 +1,191 @@
 import {useState} from "react";
 
 export type IndexUIProps = {
-    numberOfItem: number | undefined,
-    itemDetail: any,
-    capacity: number | undefined,
-    setNumberOfItem: any,
-    setItemDetail: any,
-    setCapacity: any,
-    register: any,
-    handleSubmit: any,
-    watch: any,
-    errors?: any,
-    onFirstSubmit: any,
-    onSecondSubmit: any,
-    isChosen : boolean[],
+	bestValueByCuckoo: number | undefined,
+	bestWeightByCuckoo: number | undefined,
+	capacity: number | undefined,
+	errors?: any,
+	handleSubmit: any,
+	isChosenByCuckoo: boolean[],
+	itemDetail: any,
+	numberOfItem: number | undefined,
+	onFirstSubmit: any,
+	onSecondSubmit: any,
+	register: any,
+	setCapacity: any,
+	setItemDetail: any,
+	setNumberOfItem: any,
+	watch: any,
+	isChosenByReal: boolean[],
+	bestValueByReal: number | undefined,
+	bestWeightByReal: number | undefined,
+	timeByCuckoo: number | undefined,
+	timeByReal: number | undefined
 }
-export type ItemProps={
-    value: number,
-    weight: number
+export type ItemProps = {
+	value: number,
+	weight: number
 }
-export default function IndexUI(props: IndexUIProps)
-{
-    const {numberOfItem, itemDetail, capacity, setNumberOfItem, setItemDetail, setCapacity, register, handleSubmit, watch, errors, onFirstSubmit, onSecondSubmit, isChosen} = {...props}
-    return(
-        <>
-            <form >
-                <div className="flex gap-4">
-                    <label htmlFor="numberOfItem">Number of item</label>
-                    <input className={'border-2 w-1/2'} type="number" id="numberOfItem" name="numberOfItem" {...register("numberOfItem", {required: true, min: 1, max: 100, setValueAs: (v:string) => parseInt(v) })}/>
-                    {errors.numberOfItem && <span className="text-red-500">This field is required</span>}
-                </div>
-                <div className="flex gap-4">
-                    <label htmlFor="capacity">Capacity</label>
-                    <input className={'border-2 w-1/2'} type="number" id="capacity" name="capacity" {...register("capacity", {required: true, min: 1, max: 100, setValueAs: (v:string) => parseInt(v)})} />
-                    {errors.capacity && <span className="text-red-500">This field is required</span>}
-                </div>
-            </form>
-            <div className="flex gap-4">
-                <button className={'p-1 border-4'} type='submit' onClick={handleSubmit(onFirstSubmit)}>
-                    OK
-                </button>
+export default function IndexUI(props: IndexUIProps) {
+	const {
+		numberOfItem,
+		itemDetail,
+		capacity,
+		setNumberOfItem,
+		setItemDetail,
+		setCapacity,
+		register,
+		handleSubmit,
+		watch,
+		errors,
+		onFirstSubmit,
+		onSecondSubmit,
+		isChosenByCuckoo,
+		bestWeightByCuckoo,
+		bestValueByCuckoo,
+		bestWeightByReal,
+		bestValueByReal,
+		isChosenByReal,
+		timeByReal,
+		timeByCuckoo
+	} = {...props}
+	return (
+		<>
+			<div className={'w-full flex justify-center text-2xl mb-4'}> Cuckoo Search for knapsack problem </div>
+			<form className={'flex flex-col items-center'}>
+				<div className="flex gap-4">
+					<label htmlFor="numberOfItem" className={'w-40'}> Số món đồ </label>
+					<input className={'border-2 w-20'} type="number" id="numberOfItem"
+								 name="numberOfItem" {...register("numberOfItem", {
+						required: true,
+						min: 1,
+						max: 100,
+						setValueAs: (v: string) => parseInt(v)
+					})}/>
+					{errors.numberOfItem && <span className="text-red-500">This field is required</span>}
+				</div>
+				<div className="flex gap-4">
+					<label htmlFor="capacity" className={'w-40'}> Khối lượng tối đa </label>
+					<input className={'border-2 w-20'} type="number" id="capacity" name="capacity" {...register("capacity", {
+						required: true,
+						min: 1,
+						max: 100,
+						setValueAs: (v: string) => parseInt(v)
+					})} />
+					{errors.capacity && <span className="text-red-500">This field is required</span>}
+				</div>
+			</form>
+			<div className="w-full flex gap-4 justify-center">
+				<button className={'p-2 rounded-xl border-2 hover:bg-green-500 transition-all duration-300'} type='submit' onClick={handleSubmit(onFirstSubmit)}>
+					OK
+				</button>
+			</div>
+			{numberOfItem && capacity &&
+        <div className={'w-full flex flex-col items-center'}>
+          <form>
+						{Array.from(Array(numberOfItem).keys()).map((item, index) => {
+							const [itemDetails2, setItemDetails2] = useState({value: 0, weight: 0})
+							itemDetail[index] = itemDetails2
+							return (
+								<div className={`flex justify-around gap-4`} key={index}>
+									<div className="flex gap-4 my-2">
+										<label>Item {index + 1}</label>
+									</div>
+									<div className="flex gap-4 my-2">
+										<label htmlFor={`Value${index}`}>Trị giá:</label>
+										<input className={'border-2 w-20'} type="number" id={`Value${index}`} name={`Value${index}`}
+													 onChange={event => {
+														 setItemDetails2({value: parseInt(event.target.value), weight: itemDetails2.weight})
+													 }}/>
+										<label htmlFor={`Weight${index}`}>Khối lượng:</label>
+										<input className={'border-2 w-20'} type="number" id={`Weight${index}`} name={`Weight${index}`}
+													 onChange={event => {
+														 setItemDetails2({value: itemDetails2.value, weight: parseInt(event.target.value)})
+													 }}/>
+									</div>
+								</div>
+							)
+						})}
+            <div className={'flex justify-around gap-4'}>
+              <label htmlFor={`capacity`}>Số lượng tổ cuckoo:</label>
+              <input defaultValue={50} className={'border-2 w-20'} type="number" id={`nest`}
+                     name={`nest`} {...register('nest', {required: true, setValueAs: (v: string) => parseInt(v)})}/>
             </div>
-            {numberOfItem && capacity &&
+            <div className={'flex justify-around gap-4'}>
+              <label htmlFor={'pa'}>Xác suất bị bỏ rơi </label>
+              <input className={'border-2 w-20'} type="number" defaultValue={0.25} {...register('pa', {
+								required: true,
+								setValueAs: (v: string) => parseFloat(v)
+							})}/>
+            </div>
+            <div className={'flex justify-around gap-4'}>
+              <label htmlFor={'iter'}>Số vòng lặp</label>
+              <input className={'border-2 w-20'} type="number" defaultValue={100} {...register('iter', {
+								required: true,
+								setValueAs: (v: string) => parseInt(v)
+							})}/>
+            </div>
+          </form>
+          <div className="flex gap-4">
+            <button className={'p-2 rounded-xl border-2 hover:bg-green-500 transition-all duration-300'} type='submit' onClick={handleSubmit(onSecondSubmit)}>
+              OK
+            </button>
+          </div>
+        </div>
+			}
+			{bestValueByCuckoo && bestWeightByCuckoo &&
+        <div className={'flex flex-col items-center'}>
+          <div className={'flex gap-4'}>
+						{/*Cuckoo*/}
+            <div className={'p-4 border-2 border-black'}>
               <div>
-                <form>
-                    {Array.from(Array(numberOfItem).keys()).map((item, index) => {
-                        const [itemDetails2, setItemDetails2] = useState({value: 0, weight: 0})
-                        itemDetail[index] = itemDetails2
-                        return (
-                            <div className={`flex gap-4 ${isChosen.length === numberOfItem && !isChosen[index] && 'bg-red-500'}`} key={index}>
-                                <div className="flex gap-4 my-2" >
-                                    <label>Item {index+1}</label>
-                                </div>
-                                <div className="flex gap-4 my-2" >
-                                    <label htmlFor={`Value${index}`}>Value:</label>
-                                    <input className={'border-2 w-20'} type="number" id={`Value${index}`} name={`Value${index}`}
-                                           onChange={ event => {setItemDetails2({value: parseInt(event.target.value), weight: itemDetails2.weight})}} />
-                                    <label htmlFor={`Weight${index}`}>Weight:</label>
-                                    <input className={'border-2 w-20'} type="number" id={`Weight${index}`} name={`Weight${index}`}
-                                           onChange={ event => {setItemDetails2({value: itemDetails2.value, weight: parseInt(event.target.value)})}} />
-                                </div>
-                            </div>
-                        )
-                    })}
-                </form>
-                <div className="flex gap-4">
-                    <button className={'p-1 border-4'} type='submit' onClick={handleSubmit(onSecondSubmit)}>
-                        OK
-                    </button>
-                </div>
+                Cuckoo
               </div>
-            }
-        </>
-    )
+              <div>
+                <label>Best Value: {bestValueByCuckoo}</label>
+              </div>
+              <div>
+                <label>Best Weight: {bestWeightByCuckoo}</label>
+              </div>
+              <div>
+                <label>Time: {timeByCuckoo}</label>
+              </div>
+            </div>
+
+						{/*Real*/}
+            <div className={'p-4 border-2 border-black'}>
+              <div>
+                Real
+              </div>
+              <div>
+                <label>Best Value: {bestValueByReal}</label>
+              </div>
+              <div>
+                <label>Best Weight: {bestWeightByReal}</label>
+              </div>
+              <div>
+                <label>Time: {timeByReal}</label>
+              </div>
+            </div>
+          </div>
+					<div className={'flex flex-col gap-4 my-4'}>
+						{Array.from(Array(numberOfItem).keys()).map((item, index) => {
+							return (
+								<div className={`flex gap-4`} key={index}>
+									<div className={`${isChosenByCuckoo[index] ? 'bg-green-600' : 'bg-red-600'}`}>
+										Cuckoo
+									</div>
+									<div className="flex gap-4 my-2">
+										<label>Item {index + 1}</label>
+									</div>
+									<div className={`${isChosenByReal[index] ? 'bg-green-600' : 'bg-red-600'}`}>
+										Real
+									</div>
+								</div>
+						)})}
+					</div>
+        </div>
+			}
+		</>
+	)
 }
--- app/page.tsx
+++ app/page.tsx
@@ -1,25 +1,71 @@
 "use client"
-import Image from 'next/image'
 import {useState} from "react";
 import {useForm} from "react-hook-form";
 import IndexUI from "@/app/UI";
 import {IndexUIProps} from "@/app/UI";
+
 export default function Home() {
-  const {register, handleSubmit, watch, formState: {errors}} = useForm()
-  const [ numberOfItem, setNumberOfItem] = useState(undefined)
-  const [ itemDetail, setItemDetail] = useState([])
-  const [ capacity, setCapacity] = useState(undefined)
-  const [ isChosen, setIsChosen] = useState<boolean[]>([])
-  const onFirstSubmit = (data: any) => {
-    setNumberOfItem(data.numberOfItem)
-    setCapacity(data.capacity)
-  }
-  const onSecondSubmit = (data: any) => {
-    console.log(itemDetail)
-  }
-  return (
-      <>
-        <IndexUI numberOfItem={numberOfItem} itemDetail={itemDetail} capacity={capacity} setNumberOfItem={setNumberOfItem} setItemDetail={setItemDetail} setCapacity={setCapacity} register={register} handleSubmit={handleSubmit} watch={watch} errors={errors} onFirstSubmit={onFirstSubmit} onSecondSubmit={onSecondSubmit} isChosen={isChosen}/>
-      </>
-  )
+	const {register, handleSubmit, watch, formState: {errors}} = useForm()
+	const [numberOfItem, setNumberOfItem] = useState(undefined)
+	const [itemDetail, setItemDetail] = useState([])
+	const [capacity, setCapacity] = useState(undefined)
+	const [isChosenByCuckoo, setIsChosenByCuckoo] = useState<boolean[]>([])
+	const [bestValueByCuckoo, setBestValueByCuckoo] = useState<number | undefined>(undefined)
+	const [bestWeightByCuckoo, setBestWeightByCuckoo] = useState<number | undefined>(undefined)
+	const [timeByCuckoo, setTimeByCuckoo] = useState<number | undefined>(undefined)
+	const [isChosenByReal, setIsChosenByReal] = useState<boolean[]>([])
+	const [bestValueByReal, setBestValueByReal] = useState<number | undefined>(undefined)
+	const [bestWeightByReal, setBestWeightByReal] = useState<number | undefined>(undefined)
+	const [timeByReal, setTimeByReal] = useState<number | undefined>(undefined)
+
+	const onFirstSubmit = (data: any) => {
+		setNumberOfItem(data.numberOfItem)
+		setCapacity(data.capacity)
+	}
+	const onSecondSubmit = async (data: any) => {
+		const val: number[] = []
+		const weight: number[] = []
+		if (numberOfItem) {
+			for (let i = 0; i < numberOfItem; i++) {
+				val.push(itemDetail[i].value)
+				weight.push(itemDetail[i].weight)
+			}
+		}
+		const res = await fetch('http://localhost:8000/api', {
+			method: 'POST',
+			body: JSON.stringify({
+				val: val,
+				weight: weight,
+				capacity: capacity,
+				N: data.nest,
+				pa: data.pa,
+				maxiter: data.iter
+			}),
+			headers: {
+				'Content-Type': 'application/json'
+			},
+			mode: 'cors'
+		})
+		const result = await res.json()
+		setIsChosenByCuckoo(result.cuckoo_sol)
+		setBestValueByCuckoo(result.cuckoo_val)
+		setBestWeightByCuckoo(result.cuckoo_weight)
+		setTimeByCuckoo(result.cuckoo_time)
+
+		setIsChosenByReal(result.real_sol)
+		setBestValueByReal(result.real_val)
+		setBestWeightByReal(result.real_weight)
+		setTimeByReal(result.real_time)
+	}
+	return (
+		<>
+			<IndexUI numberOfItem={numberOfItem} itemDetail={itemDetail} capacity={capacity} setNumberOfItem={setNumberOfItem}
+							 setItemDetail={setItemDetail} setCapacity={setCapacity} register={register} handleSubmit={handleSubmit}
+							 watch={watch} errors={errors} onFirstSubmit={onFirstSubmit} onSecondSubmit={onSecondSubmit}
+							 isChosenByCuckoo={isChosenByCuckoo} bestValueByCuckoo={bestValueByCuckoo} bestWeightByCuckoo={bestWeightByCuckoo}
+								isChosenByReal={isChosenByReal} bestValueByReal={bestValueByReal} bestWeightByReal={bestWeightByReal}
+							 timeByCuckoo={timeByCuckoo} timeByReal={timeByReal}
+			/>
+		</>
+	)
 }
